eb745af12415f5a9d58c39897c07b1aa65ca28ff
KINGXDENI <96621497+KINGXDENI@users.noreply.github.com>
2022-07-25 23:57:53 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,10 +3,10 @@
     date_default_timezone_set("ASIA/JAKARTA");
 
     // deklarasi parameter koneksi database
-    $server = "localhost"; // server database, default “localhost” atau “127.0.0.1”
-    $username = "root"; // username database, default “root”
-    $password = ""; // password database, default kosong
-    $database = "db_sekolah"; // memilih database yang akan digunakan
+    $server = "www.db4free.net"; // server database, default “localhost” atau “127.0.0.1”
+    $username = "muhdenisetiawan"; // username database, default “root”
+    $password = "123dendut"; // password database, default kosong
+    $database = "db_sekolahdeni"; // memilih database yang akan digunakan
 
     // koneksi database
     $db = mysqli_connect($server, $username, $password, $database);
